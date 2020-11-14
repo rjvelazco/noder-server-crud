33c115923492023622bae35b49451c5f387ba9f8
rjvelazco <rjvelazco21@gmail.com>
2020-11-14 19:34:52 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV == 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://rjvelazco:oRh5xMmsedWVImwO@cluster0.u7v4j.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
